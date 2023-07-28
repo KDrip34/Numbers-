953c67f3f4b3956e35365937d010a0b9e4fc4393
KDrip34 <124319502+KDrip34@users.noreply.github.com>
2023-07-28 15:52:22 -0700

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,15 @@
+person = {'first': 'Kendrick',
+		'last': 'Ekejiuba', 
+		'city' : 'San Leandro',
+		}
+print(person)
+numbers = {'kendrick' : '34',
+			'alex' : '2311',
+			'jaun' : '6',
+			'adrian' : '30'	,
+			}
+print("Kendrick's favorite number is " + numbers['kendrick'].title() + ".") 			
+print("Alex's favorite number is " + numbers['alex'].title() + ".") 	
+print("Jauns's favorite number is " + numbers['jaun'].title() + ".") 	
+print("Adrian's favorite number is " + numbers['adrian'].title() + ".") 
+print("...")
